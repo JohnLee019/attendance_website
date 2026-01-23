a42680a9e7ee5eaac2ffe99f59b1c4630b3cf3cc
Hongmin <ohno0601111@gmail.com>
2026-01-23 14:10:54 +0900

    date of the attendance

---
+++ src/date.ts
@@ -0,0 +1,10 @@
+function getTodayDate() {
+    const date = document.getElementById("date");
+    if (!date) return;
+
+    const today: Date = new Date();
+    const year: number = today.getFullYear();
+    const month: string = (today.getMonth()+1).toString().padStart(2, '0');
+    const day: string = today.getDate().toString().padStart(2, '0');
+    date.textContent = `${month}/${day}/${year}`;
+}
