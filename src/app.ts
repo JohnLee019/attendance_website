bcfa60f858876b524e1c68e57ef35c5230e1483f
Hongmin <ohno0601111@gmail.com>
2026-01-25 15:59:56 +0900

    db를 table.ts로 연결하기 완료

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,23 @@
 import express from "express";
 import { db } from "./db";
+import path from "path";
 
 const app = express();
 app.use(express.json());
+app.use(express.static(path.join(process.cwd(), "public")));
+app.use("/dist", express.static(path.join(process.cwd(), "dist")));
 
-app.get("/seats", (_req, res) => {
-  const rows = db.prepare(`SELECT id, seatNo FROM Seat ORDER BY seatNo ASC;`).all();
+
+app.get("/api/seats", (_req, res) => {
+  const rows = db.prepare(`
+    SELECT
+    s.id, s.seatNo, s.personId, s.r, s.c, s.rs, s.cs, p.name  AS personName, p.color AS personColor 
+    FROM Seat s LEFT JOIN Person p ON p.id = s.personId
+    ORDER BY s.seatNo ASC
+  `).all();
   res.json(rows);
 });
 
 app.listen(3000, () => {
   console.log("Server running on http://localhost:3000");
-  console.log("GET seats: http://localhost:3000/seats");
 });
